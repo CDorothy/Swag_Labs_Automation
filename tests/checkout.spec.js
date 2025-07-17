b7e6b89e1ba20c31958419f1e82076efaa8d0ad6
Darothi Chowdhury <darothi.chowdhury@binarysemantics.com>
2025-07-17 18:51:53 +0530

    checkout code

--- tests/checkout.spec.js
+++ tests/checkout.spec.js
@@ -3,6 +3,9 @@ import { loginPage } from '../pages/loginPage';
 import { productPage } from '../pages/productPage';
 import { cartPage } from '../pages/cartPage';
 import { checkoutPage } from '../pages/checkoutPage';
+import logindata from '../testdata/loginData.json';
+import productdata from '../testdata/productData.json';
+import cartdata from '..//testdata/cartData.json';
 import checkoutdata from '../testdata/checkoutData.json';
 
 test('testing checkout page', async({page}) => {
@@ -39,22 +42,26 @@ test('testing checkout page', async({page}) => {
     await expect(page.locator(cart.productBackBtn)).toBeVisible(); //verify if backbutton is present
 
     await cart.goBackToCart();
-    await expect(page.locator(cart.shoppingCart)).toBeVisible();
-    await expect(page.locator(cart.removeBtn)).toBeVisible();
-    await expect(page.locator(cart.checkoutBtn)).toBeVisible();
+    await expect(page.locator(cart.shoppingCart)).toBeVisible(); //verify link is visible
+    await expect(page.locator(cart.removeBtn)).toBeVisible(); //verify remove button is visible
+    await expect(page.locator(cart.checkoutBtn)).toBeVisible(); //verify checkout button is visible
 
     await cart.verifyCart();
 
-    await expect(page).toHaveURL(cartdata.checkouturl);
+    await expect(page).toHaveURL(cartdata.checkouturl); //verify checkout url
 
     await page.waitForTimeout(2000);
 
     const checkout = new checkoutPage(page);
 
-    await checkout.goBackToCartPage();
-    await expect(checkout.cartText).toHaveText("Your Cart");
+    await checkout.goBackToCartPage(); //verify navigating back to cart page
+    await expect(page.locator(checkout.cartText)).toHaveText("Your Cart"); //verify title of the cart page
 
     await checkout.goBackToCheckOutPage();
+    await expect(page.locator(checkout.titleCheckoutPage)).toHaveText(checkoutdata.title); //verify checkout page title
 
-    await checkout.fillCheckoutPage(checkoutdata.firstname, checkoutdata.lastname, checkoutdata.zipcode);
+    await checkout.fillCheckoutPage(checkoutdata.firstname, checkoutdata.lastname, checkoutdata.zipcode); //fill checkout form
+    await expect(page).toHaveURL(checkoutdata.url); // verify overview url
+
+    await page.close();
 })
